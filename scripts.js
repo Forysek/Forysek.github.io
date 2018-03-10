d313458bf9ff72e0759aa145f6744b63a7c22572
Forysek <foryssek@gmail.com>
2018-03-10 19:02:56 +0100

    commit

--- scripts.js
+++ scripts.js
@@ -122,6 +122,7 @@ $(document).ready(function() {
   }
 
   function handleTaskSubmitRequest(event) {
+    event.preventDefault();
 
     var taskTitle = $(this).find('[name="title"]').val();
     var taskContent = $(this).find('[name="content"]').val();
@@ -138,8 +139,8 @@ $(document).ready(function() {
         title: taskTitle,
         content: taskContent
       }),
-      success: getAllTasks
     });
+    getAllTasks();
   }
 
   function toggleEditingState() {
