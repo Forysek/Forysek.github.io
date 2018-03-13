d1c5afe22e3a31c66d4beac49e079a2b827e46aa
Forysek <foryssek@gmail.com>
2018-03-13 20:27:29 +0100

    commit

--- scripts.js
+++ scripts.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
     $.ajax({
       url: requestUrl,
       method: 'GET',
-      contentType: "application/json",
+      // contentType: "application/json",
       success: function(tasks) {
         tasks.forEach(task => {
           availableTasks[task.id] = task;
