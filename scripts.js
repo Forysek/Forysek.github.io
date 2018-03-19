c15f40c2aef0c1455a3745dd2a0931f534322785
Forysek <foryssek@gmail.com>
2018-03-19 19:36:33 +0100

    commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://dry-temple-20838.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://dry-temple-20838.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://dry-temple-20838.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://dry-temple-20838.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
